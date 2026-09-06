6866ede4f2d0e50f6109fa3585313718785350e9
Wellinbox <143895408+wellinbox@users.noreply.github.com>
2026-09-07 00:47:08 +0700

    feat: use attached images as UI asset sheets only

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./styles/global.css";
+import "./styles/assets.css";
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
