bdb8985e973ae43f431683a0da388dc16752df69
Wellinbox <143895408+wellinbox@users.noreply.github.com>
2026-09-07 13:12:57 +0700

    connect splash toast and status in App

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -17,6 +17,7 @@ import "./styles/settings.css";
 import "./styles/wallet.css";
 import "./styles/broke.css";
 import "./styles/landing.css";
+import "./styles/toast.css";
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
